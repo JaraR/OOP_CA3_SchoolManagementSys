fa6b1d6ad331b8fc3366eab752ca59e73e4418e1
JaraR <jara_r1993@yahoo.com>
2023-05-09 17:09:48 -0500

    English Student class
    Added an english student class that inherits from the student class.

--- SchoolManagmentSystem.py
+++ SchoolManagmentSystem.py
@@ -12,6 +12,21 @@ class Student(object):
         return "ID: " + str(self.student_id) + " Name: " + self.first + " " + self.last
 
 
+class EnglishStudent(Student):
+    """English student class that inherits from student class"""
+
+    def __init__(self, student_id, first, last, english_grade):
+        """initializes an English student with an ID, first name, last name, and an english grade"""
+        Student.__init__(self, student_id, first, last)
+        self.english_grade = english_grade
+
+    def __str__(self):
+        """returns a student's ID, first name, last name, and English grade"""
+        result_str = Student.__str__(self)
+        result_str = result_str + " English grade: " + str(self.english_grade)
+        return result_str
+
+
 # Main Program
 student1 = Student(1, "John", "Doe")
 print(student1)
