35d8c30811ccd666ef51521dbdc88da37eaa403c
JaraR <jara_r1993@yahoo.com>
2023-05-09 17:44:44 -0500

    History Student class
    Added a history student class that inherits from the student class.

--- SchoolManagmentSystem.py
+++ SchoolManagmentSystem.py
@@ -16,7 +16,7 @@ class EnglishStudent(Student):
     """English student class that inherits from student class"""
 
     def __init__(self, student_id, first, last, english_grade):
-        """initializes an English student with an ID, first name, last name, and an english grade"""
+        """initializes an English student with an ID, first name, last name, and an English grade"""
         Student.__init__(self, student_id, first, last)
         self.english_grade = english_grade
 
@@ -24,6 +24,21 @@ class EnglishStudent(Student):
         """returns a student's ID, first name, last name, and English grade"""
         result_str = Student.__str__(self)
         result_str = result_str + " English grade: " + str(self.english_grade)
+        return result_str
+
+
+class HistoryStudent(Student):
+    """History student class that inherits from student class"""
+
+    def __init__(self, student_id, first, last, history_grade):
+        """initializes a History student with an ID, first name, last name, and a History grade"""
+        Student.__init__(self, student_id, first, last)
+        self.history_grade = history_grade
+
+    def __str__(self):
+        """returns a student's ID, first name, last name, and History grade"""
+        result_str = Student.__str__(self)
+        result_str = result_str + " History grade: " + str(self.history_grade)
         return result_str
 
 
