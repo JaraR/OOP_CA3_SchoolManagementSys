a105d1f482153c2cfd96fc086289f983104a10a3
JaraR <jara_r1993@yahoo.com>
2023-05-09 11:40:11 -0500

    Student class added
    Barebone class implemented to start with.

--- SchoolManagmentSystem.py
+++ SchoolManagmentSystem.py
@@ -1,1 +1,13 @@
-class Student()
+class Student(object):
+    def __init__(self, ID, first, last):
+        """initializes a student with an ID, first name, and last name"""
+        self.ID = ID
+        self.first = first
+        self.last = last
+
+    def __str__(self):
+        return "ID: " + str(self.ID) + " Name: " + self.first + " " + self.last
+
+
+student1 = Student(1, "John", "Doe")
+print(student1)
