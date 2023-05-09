48c9e3d65f5e39903e646cb4d64949fab2b335af
JaraR <jara_r1993@yahoo.com>
2023-05-09 14:51:00 -0500

    Docstrings
    Added docstrings to the student class and some other small changes.

--- SchoolManagmentSystem.py
+++ SchoolManagmentSystem.py
@@ -1,13 +1,17 @@
 class Student(object):
-    def __init__(self, ID, first, last):
+    """student class that is then inherited by English, History, and Math student class"""
+
+    def __init__(self, student_id, first, last):
         """initializes a student with an ID, first name, and last name"""
-        self.ID = ID
+        self.student_id = student_id
         self.first = first
         self.last = last
 
     def __str__(self):
-        return "ID: " + str(self.ID) + " Name: " + self.first + " " + self.last
+        """returns a student's ID, first name, and last name"""
+        return "ID: " + str(self.student_id) + " Name: " + self.first + " " + self.last
 
 
+# Main Program
 student1 = Student(1, "John", "Doe")
 print(student1)
